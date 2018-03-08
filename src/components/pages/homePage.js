125b1eec7ee8e289d5dd14b0e82743937d6de7bd
Mitri <maboudsy@ucsc.edu>
2018-03-08 10:52:44 -0800

    uploading to master

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import Dropzone from 'react-dropzone';
+import axios from 'axios';
 
 class Homepage extends Component {
+  state = {
+    selectedFile: null
+  }
+
+  fileSelectedHandler = event =>{
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+    axios.post('gs://printer-1c171.appspot.com', fd);
+  }
   render() {
     return (
       <div className="container-fluid">
@@ -10,56 +25,27 @@ class Homepage extends Component {
       Welcome to 3D Print!
       </h1>
         <div className = "content">
-          <div className="backImage">
-            <div className = "picture">
-              <img src={require('./world2.png')} alt = "graph"/>
-            </div>
-            <div className = "text">
-             <p>
-              3D Print hosts a platform that can reward designers, makers, and buyers for participating in the
-              emerging global 3D printing market. We want to encourage surf board fin production
-              to empower surfers and local communities.
-              </p>
-            </div>
+          <div className = "picture">
+            <img src={require('./world2.png')} alt = "graph"/>
           </div>
-        </div>
-<<<<<<< HEAD
-       class UploadScreen extends Component {
-render() {
-    return (
-     <div className="App">
-      <Dropzone onDrop={(files) => this.onDrop(files)}>
-                <div>Try dropping some files here, or click to select files to upload.</div>
-          </Dropzone>
-     </div>
-     )
- }
-}
-        //new page here************************************************8
-=======
-        {/*//new page here********************************************** */}
->>>>>>> 8636e15c34b14d5e7a139cf7c7ad9b442f1c407a
-        <div className = "content">
-          <div className="backImage">
-            <div className = "picture">
-            
+          <div className = "text">
+            <p>
+            3D Print hosts a platform that can reward designers, makers, and buyers for participating in the
+            emerging global 3D printing market. We want to encourage surf board fin production
+            to empower surfers and local communities.
+            </p>
             </div>
-            <div className = "text">
-              <p>
-              3D Print hosts a platform that can reward designers, makers, and buyers for participating in the
-              emerging global 3D printing market. We want to encourage surf board fin production
-              to empower surfers and local communities.
-              </p>
-            </div>
-          </div>
+    
         </div>
 
-
-
+          <input type="file" onChange={this.fileSelectedHandler}/>
+          <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
 
     );
+
   }
+
 }
 
 export default Homepage;
