706db9e765b155b963efc02b6096b17d240792ad
Kevin Crum <kevincrum@eduroam-169-233-214-133.ucsc.edu>
2018-03-08 12:12:46 -0800

    temp remove db lines for compilation

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,9 @@
 import * as auth from './auth';
 import * as firebase from './firebase';
-import * as database from './database';
+//import * as database from './database';
 
 export{ 
   auth,
   firebase, 
-  database,
+  //database,
 };
