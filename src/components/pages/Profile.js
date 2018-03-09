1d1eae29737095cde750b1a1a8c92173c3c25642
Kevin Crum <kevinbaileycrum@gmail.com>
2018-03-08 19:30:26 -0800

    Profile page is protected, signed out users are redirected to sign in

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { firebase } from '../../firebase/index';
+import withAuthorization from '../../withAuthorization';
 //import { PasswordForgetForm } from '../PasswordForget';
 //import PasswordChangeForm from '../PasswordChange';
 //import withAuthorization from '../Session/withAuthorization';
 /*
+
+
+/*
+class Profile extends Component{
+
+  //firebase.auth().currentUser
+
+  render(){
+    return(
+      
+    );
+  }
+}
+
+export default Profile;
+
 const AccountPage = (props, { authUser }) =>
   <div>
     <h1>Account: {authUser.email}</h1>
@@ -22,17 +39,19 @@ export default withAuthorization(authCondition)(
 AccountPage);
 */
 
-class Profile extends Component{
 
-  //firebase.auth().currentUser
-
-  render(){
-    return(
-      <h1>
+const Profile = (props, { authUser }) =>
+  <div>
+    <h1>
         welcome to Profile, dont get caught in the storm
-      </h1>
-    );
-  }
-}
+    </h1>
+    <p1> this page will be protected </p1>
+  </div>
 
-export default Profile;
+Profile.contextTypes = {
+  authUser: PropTypes.object,
+};
+
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(Profile);
