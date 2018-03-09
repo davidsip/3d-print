0253867696f796a446b03b1148842a4d36a4eee3
Kevin Crum <kevinbaileycrum@gmail.com>
2018-03-08 19:21:11 -0800

    cleaned up SignOut

--- src/components/pages/SignOut.js
+++ src/components/pages/SignOut.js
@@ -1,14 +1,10 @@
 // 
 // SignOut.js
 // 
-//import * as routes from '../../constants/routes';
+
 import React from 'react';
 import { auth } from '../../firebase/';
 import { Link } from 'react-router-dom'
-//import PropTypes from 'prop-types';
-//import withAuthorization from '../../withAuthorization';
-
-
 
 const SignOutButton = () => 
   <div className = "sOutButton">
