ee423f553953b5c1a13b37ca5741030eb49de19f
Kevin Crum <kevinbaileycrum@gmail.com>
2018-03-11 21:40:29 -0700

    updated firebase.js
    removed our specific keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config={
-    apiKey: "AIzaSyD8uoLEajmdN8rw8EDoIKggOHSDAJg8YXg",
-    authDomain: "printer-1c171.firebaseapp.com",
-    databaseURL: "https://printer-1c171.firebaseio.com",
-    projectId: "printer-1c171",
-    storageBucket: "printer-1c171.appspot.com",
-    messagingSenderId: "198617925096"
+    apiKey: " see console ",
+    authDomain: " see console ",
+    databaseURL: " see console ",
+    projectId: " see console ",
+    storageBucket: " see console ",
+    messagingSenderId: " see console ",
 };
 // is this getting called
 
