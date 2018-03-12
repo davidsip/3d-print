cfff1de8929ffb10ac598491ff17bbcaa64a1c45
Mitri <maboudsy@ucsc.edu>
2018-03-12 06:12:04 -0700

    added image slideshow feature

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -25,9 +25,6 @@ class Homepage extends Component {
       Welcome to 3D Print!
       </h1>
         <div className = "content">
-          <div className = "picture">
-            <img src={require('./world2.png')} alt = "graph"/>
-          </div>
           <div className = "text">
             <p>
             3D Print hosts a platform that can reward designers, makers, and buyers for participating in the
@@ -37,6 +34,33 @@ class Homepage extends Component {
             </div>
     
         </div>
+
+          <div class ="wrap">
+
+              <input type="radio" class="rnav black" id="slide_1" name="coins"/>
+              <input type="radio" class="rnav red" id="slide_2" name="coins"/>
+              <input type="radio" class="rnav yellow" id="slide_3" name="coins"/>
+
+              <div class="boxes">
+                  <div class="box black">
+                      <img src={require('./world2.png')} alt=""/>
+                  </div>
+                  <div class="box red">
+                      <img src={require('./bb.jpg')} alt=""/>
+                  </div>
+                  <div class="box yellow">
+                      <img src={require('./aa.jpg')} alt=""/>
+                  </div>
+              </div>
+
+              <div class="labels">
+                  <label for="slide_1" class="s-index b-label"></label>
+                  <label for="slide_2" class="s-index r-label"></label>
+                  <label for="slide_3" class="s-index y-label"></label>
+              </div>
+          </div>
+
+          
 
           <input type="file" onChange={this.fileSelectedHandler}/>
           <button onClick={this.fileUploadHandler}>Upload</button>
