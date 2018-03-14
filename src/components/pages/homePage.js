105901c3e82c4b2470cf86eeecaa0dd766d0ae56
davidsip <davidsip@outlook.com>
2018-03-14 14:24:43 -0700

    uploaded deleted upload

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -17,15 +17,6 @@ class Homepage extends Component {
     fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
     axios.post('gs://printer-1c171.appspot.com', fd);
   }
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
-
-
   render() {
     return (
       <div className="container-fluid">
@@ -40,34 +31,41 @@ class Homepage extends Component {
             emerging global 3D printing market. We want to encourage surf board fin production
             to empower surfers and local communities.
             </p>
-          </div>
+            </div>
 
 
           <div class ="wrap">
+
               <input type="radio" class="rnav black" id="slide_1" defaultChecked name="coins"/>
               <input type="radio" class="rnav red" id="slide_2" name="coins"/>
               <input type="radio" class="rnav yellow" id="slide_3" name="coins"/>
 
-                  <div class="boxes">
-                      <div class="box black">
-                          <img src={require('./world2.png')} alt=""/>
-                      </div>
-                      <div class="box red">
-                          <img src={require('./bb.jpg')} alt=""/>
-                      </div>
-                      <div class="box yellow">
-                          <img src={require('./aa.jpg')} alt=""/>
-                      </div>
+              <div class="boxes">
+                  <div class="box black">
+                      <img src={require('./world2.png')} alt=""/>
                   </div>
-                  <div class="labels">
-                      <label for="slide_1" class="s-index b-label"></label>
-                      <label for="slide_2" class="s-index r-label"></label>
-                      <label for="slide_3" class="s-index y-label"></label>
+                  <div class="box red">
+                      <img src={require('./bb.jpg')} alt=""/>
                   </div>
+                  <div class="box yellow">
+                      <img src={require('./aa.jpg')} alt=""/>
+                  </div>
+              </div>
+
+              <div class="labels">
+                  <label for="slide_1" class="s-index b-label"></label>
+                  <label for="slide_2" class="s-index r-label"></label>
+                  <label for="slide_3" class="s-index y-label"></label>
+              </div>
           </div>
 
         </div>
 
+
+
+
+          <input type="file" onChange={this.fileSelectedHandler}/>
+          <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
 
     );
